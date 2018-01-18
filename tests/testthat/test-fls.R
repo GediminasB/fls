bd4ea07964e6d5c2be04d406f572a7b37a008b3c
Gediminas Bagdonas <gedbagdonas@gmail.com>
2018-01-18 08:32:25 +0200

    Updated tests for main function

--- tests/testthat/test-fls.R
+++ tests/testthat/test-fls.R
@@ -1,12 +1,15 @@
+context("Testing `fls` function")
+
 X = matrix(c(1,2,3,1.01,0.99,1), ncol = 2)
 y = c(2.01,2.99,4)
 test.fls = fls(X, y, mu = 1)
 
-test_that("`fls` coef at mu = 1", {
+test_that("`fls` coef", {
   expect_identical(all(abs(coef(test.fls) - 1) < 1e015), TRUE)
 })
+
 test_that("`fls` errors", {
-  expect_identical(round(test.fls$r_D, 16), 0)
-  expect_identical(round(test.fls$r_M, 16), 0)
+  expect_equal(test.fls$r_D, 0)
+  expect_equal(test.fls$r_M, 0)
 })
 
