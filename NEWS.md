09783a25cb3a84f00c260191e721cd230a552296
Gediminas Bagdonas <gedbagdonas@gmail.com>
2018-01-18 10:06:50 +0200

    Fixed bug with Q singularity at n < K.

---
+++ NEWS.md
@@ -0,0 +1,11 @@
+# 0.1.1.9000 - 2018-01-18
+### Added
+- Added some unit tests for main function (`fls`).
+
+### Changed
+- Fixed a bug in `print.fls` for `n <= 10`.
+- Fixed error which complains about `Q` singularity at `n < K`.
+
+# 0.1.0 - 2018-01-17
+### Added
+- Initial version submitted to CRAN.
--- src/rcpp_FLS.cpp
+++ src/rcpp_FLS.cpp
@@ -20,25 +20,25 @@ arma::mat rcpp_FLS(arma::mat X, arma::vec y, double mu, bool smooth = false) {
   int K = X.n_cols;
   int n = X.n_rows;
   arma::mat Q(K, K, arma::fill::zeros);
-  arma::mat B(n, K, arma::fill::zeros);
+  arma::mat B(n, K);
+  B.fill(NA_REAL);
   arma::mat I(K, K, arma::fill::eye);
   arma::mat E(n, K, arma::fill::zeros);
   arma::colvec p = arma::zeros<arma::colvec>(K);
   arma::cube M(K, K, n, arma::fill::zeros);
-  //double r = 0;
 
   for(int i = 0; i < n; i++) {
     arma::vec p_ = p + X.row(i).t() * y(i);
     arma::mat Q_ = Q + X.row(i).t() * X.row(i);
 
-    B.row(i) = p_.t() * arma::inv(Q_);
+    if(i >= K) {
+      B.row(i) = p_.t() * arma::inv_sympd(Q_);
+    }
 
-    M.slice(i) = mu * arma::inv(Q_ + mu*I);
-    //Q = M.slice(i) * Q_;
+    M.slice(i) = mu * arma::inv_sympd(Q_ + mu*I);
     Q = mu * (I - M.slice(i));
     p = M.slice(i) * p_;
     E.row(i) = 1/mu * p.t();
-    //r = r + y(i)*y(i) - (p + X.row(i) * y(i)).t() * arma::inv(Q + X.row(i).t() * X.row(i)) * (p + X.row(i) * y(i));
   }
   if(smooth) {
     for(int i = n-2; i >= 0; i--) {
